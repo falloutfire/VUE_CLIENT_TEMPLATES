12aca836cc9909ba23063a8f76a11b859465c884
falloutfire <lihach77@gmail.com>
2019-05-28 02:25:10 +0300

    test serveer js

--- server.js
+++ server.js
@@ -1,30 +1,9 @@
 const express = require('express');
 const path = require('path');
-//const serveStatic = require('serve-static');
-const history = require('connect-history-api-fallback');
-
-
-/*let app = express();
-app.use(serveStatic(__dirname + "/dist"));
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log('Listening on port ' + port)
-});*/
+const serveStatic = require('serve-static');
 
 let app = express();
 app.use(serveStatic(__dirname + "/dist"));
-
-
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
-//app.use(staticFileMiddleware);
-
-/*app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
-});*/
 
 var server = app.listen(process.env.PORT || 3000, function () {
   var port = server.address().port;
