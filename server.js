b63b3bf078b664854663771ae9407263ef790caa
falloutfire <lihach77@gmail.com>
2019-05-28 02:19:30 +0300

    test serveer js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const serveStatic = require('serve-static');
+//const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
 
 
@@ -12,22 +12,21 @@ app.listen(port, () => {
     console.log('Listening on port ' + port)
 });*/
 
-const app = express();
+let app = express();
+app.use(serveStatic(__dirname + "/dist"));
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
-app.use(staticFileMiddleware);
 app.use(history({
   disableDotRule: true,
   verbose: true
 }));
 app.use(staticFileMiddleware);
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.render(path.join(__dirname + '/dist/index.html'));
-});
+});*/
 
-var server = app.listen(process.env.PORT || 8080, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
